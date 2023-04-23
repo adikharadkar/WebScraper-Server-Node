4e221a7c6768a9d840615ff6b1a5a7509eb5ca6a
Aditya Kharadkar <adityakharadkar27@gmail.com>
2023-04-23 18:16:54 +0530

    Created a server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
