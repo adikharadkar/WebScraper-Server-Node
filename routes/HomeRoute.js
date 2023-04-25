b0e50beb778d15ad47051d814b53a313ab3d53d8
Aditya Kharadkar <adityakharadkar27@gmail.com>
2023-04-25 13:03:28 +0530

    Commit

--- routes/HomeRoute.js
+++ routes/HomeRoute.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+// Import Error Handler
+const HttpError = require('../models/http-error');
+
 router.get('/', (req, res, next) => {
     res.status(200).send('Successful!')
 })
+
+// Demo to throw an error if any
+/*
+    if (!place) {
+        throw new HttpError('Could not find a place', 404);
+    }
+*/
 
 module.exports = router;
