22af0830a74064e9a2d99fe2a13080bb2d5e5050
Aditya Kharadkar <adityakharadkar27@gmail.com>
2023-05-03 18:43:15 +0530

    Sent a POST request to /login

--- index.js
+++ index.js
@@ -4,22 +4,16 @@ const cors = require('cors');
 
 const homeRoutes = require('./routes/HomeRoute');
 const signupRoutes = require('./routes/SingupRoute');
+const loginRoutes = require('./routes/LoginRoute');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use(cors({ origin: true }));
 
-// CORS Header => Required for cross-origin/cross-server communication
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
-
 app.use(signupRoutes);
 app.use(homeRoutes);
-
+app.use(loginRoutes);
 
 const PORT = 3001;
 
