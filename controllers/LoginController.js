c171300f6dd5196a8881f4bee05804c67c1fffee
Aditya Kharadkar <adityakharadkar27@gmail.com>
2023-05-10 00:03:34 +0530

    Update LoginController.js

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,5 +1,38 @@
-const postLoginData = (req, res, next) => {
+const HttpError = require('../models/http-error');
+const User = require('../models/user');
+const crypto = require('crypto');
+
+const postLoginData = async (req, res, next) => {
     const {email, password} = req.body;
+
+    let existingUser;
+    try {
+        existingUser = await User.findOne({email: email})
+    } catch (err) {
+        const error = new HttpError(
+            'Logging in failed, please try again later',
+            500
+        )
+        return next(error);
+    }
+
+    // Generating a random salt of 16 bytes
+    const salt = crypto.randomBytes(16).toString('hex');
+
+    // Creating a hash of a password
+    const hash = crypto.createHmac('sha256', salt)
+                        .update(password)
+                        .digest('hex');
+
+    User.findOne({email: email}).then(user => {
+        if (user) {
+            res.status(200).send(user["salt"])
+            return
+        } else {
+            res.send("Error")
+            return
+        }
+    })
 
     const loginData = {
         email,
